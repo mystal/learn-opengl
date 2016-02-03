37119928c29118a474976f0fb89e90eabd2f1c10
Gabriel Martinez <reitaka@gmail.com>
2016-02-02 22:56:48 -0800

    Draw first triangle in Hello Triangle!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+#[macro_use]
 extern crate glium;
 extern crate glutin;
 
 use glium::{DisplayBuild, Surface};
 use glutin::{Api, ElementState, Event, GlRequest, VirtualKeyCode};
+
+#[derive(Clone, Copy)]
+struct Vertex {
+    position: [f32; 3],
+}
+
+implement_vertex!(Vertex, position);
 
 fn main() {
     // Build a window with OpenGL 3.3
@@ -10,6 +18,38 @@ fn main() {
         .with_gl(GlRequest::Specific(Api::OpenGl, (3, 3)))
         .build_glium()
         .unwrap();
+
+    let shape = vec![
+        Vertex { position: [-0.5, -0.5, 0.0] },
+        Vertex { position: [0.5, -0.5, 0.0] },
+        Vertex { position: [0.0, 0.5, 0.0] },
+    ];
+
+    let vertex_buffer = glium::VertexBuffer::new(&window, &shape).unwrap();
+
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 330 core
+
+        layout (location = 0) in vec3 position;
+
+        void main() {
+            gl_Position = vec4(position.x, position.y, position.z, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 330 core
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&window, vertex_shader_src, fragment_shader_src, None).unwrap();
 
     loop {
         for event in window.poll_events() {
@@ -22,6 +62,7 @@ fn main() {
 
         let mut target = window.draw();
         target.clear_color(0.2, 0.3, 0.3, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
         target.finish().unwrap();
     }
 }
