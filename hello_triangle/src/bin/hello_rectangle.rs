3e6f888df6c930d5378833f423540f7b32bb3387
Gabriel Martinez <reitaka@gmail.com>
2016-02-13 21:24:59 -0800

    Add rectangle drawing using EBOs and indices

---
+++ hello_triangle/src/bin/hello_rectangle.rs
@@ -0,0 +1,73 @@
+#[macro_use]
+extern crate glium;
+extern crate glutin;
+
+use glium::{DisplayBuild, Surface};
+use glutin::{Api, ElementState, Event, GlRequest, VirtualKeyCode};
+
+#[derive(Clone, Copy)]
+struct Vertex {
+    position: [f32; 3],
+}
+
+implement_vertex!(Vertex, position);
+
+fn main() {
+    // Build a window with OpenGL 3.3
+    let window = glutin::WindowBuilder::new()
+        .with_gl(GlRequest::Specific(Api::OpenGl, (3, 3)))
+        .build_glium()
+        .unwrap();
+
+    let shapes = [
+        Vertex { position: [0.5, 0.5, 0.0] },
+        Vertex { position: [0.5, -0.5, 0.0] },
+        Vertex { position: [-0.5, -0.5, 0.0] },
+        Vertex { position: [-0.5, 0.5, 0.0] },
+    ];
+
+    let indices = [
+        0u8, 1, 3,
+        1, 2, 3,
+    ];
+
+    let vertex_buffer = glium::VertexBuffer::new(&window, &shapes).unwrap();
+    let index_buffer = glium::IndexBuffer::new(&window, glium::index::PrimitiveType::TrianglesList, &indices).unwrap();
+
+    let vertex_shader_src = r#"
+        #version 330 core
+
+        layout (location = 0) in vec3 position;
+
+        void main() {
+            gl_Position = vec4(position.x, position.y, position.z, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 330 core
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&window, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    loop {
+        for event in window.poll_events() {
+            match event {
+                Event::Closed => return,
+                Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::Escape)) => return,
+                _ => {},
+            }
+        }
+
+        let mut target = window.draw();
+        target.clear_color(0.2, 0.3, 0.3, 1.0);
+        target.draw(&vertex_buffer, &index_buffer, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+        target.finish().unwrap();
+    }
+}
